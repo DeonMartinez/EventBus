02787592b388bc0bc21fd411c8cb0465099d2725
DeonMartinez <49213704+DeonMartinez@users.noreply.github.com>
2020-10-16 20:30:31 -0700

    Add files via upload

---
+++ Assets/EventPublisher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPublisher : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            EventBus.TriggerEvent("Shoot");
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            EventBus.TriggerEvent("Launch");
+        }
+        if (Input.GetKeyDown("e"))
+        {
+            EventBus.TriggerEvent("Eject");
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            EventBus.TriggerEvent("Accelerate");
+        }
+        if (Input.GetKeyDown("t"))
+        {
+            EventBus.TriggerEvent("SelfDestruct");
+        }
+    }
+}
+
